beb37a590584a62befc73b5cd9be700c67a24dfd
Nitish Kumar <krnitish@live.in>
2023-01-10 23:26:03 +0530

    Creative coding: Angles

--- sketch/sketch02.js
+++ sketch/sketch02.js
@@ -4,6 +4,10 @@ const settings = {
     dimensions: [2048, 2048],
 };
 
+const degToRad = (degree) => {
+    return (degree / 180) * Math.PI;
+};
+
 const sketch = () => {
     return ({ context, width, height }) => {
         context.fillStyle = "white";
@@ -11,23 +15,36 @@ const sketch = () => {
 
         context.fillStyle = "black";
 
-        const x = width * 0.5;
-        const y = height * 0.5;
-        const w = width * 0.3;
-        const h = height * 0.3;
+        const cx = width * 0.5;
+        const cy = height * 0.5;
+        const w = width * 0.01;
+        const h = height * 0.1;
+        let x, y;
 
-        context.save();
-        context.translate(x, y);
-        context.rotate(0.3);
-        context.beginPath();
-        context.rect(-w * 0.5, -h * 0.5, w, h);
-        context.fill();
-        context.restore();
+        const num = 12;
+        const radius = width * 0.3;
 
-        context.translate(100, 400);
-        context.beginPath();
-        context.arc(0, 0, 50, 0, Math.PI * 2);
-        context.fill();
+        for (let i = 0; i < num; i++) {
+            const slice = degToRad(360 / num);
+            const angle = slice * i;
+
+            x = cx + radius * Math.sin(angle);
+            y = cy + radius * Math.cos(angle);
+
+            context.save();
+            context.translate(x, y);
+            context.rotate(-angle);
+
+            context.beginPath();
+            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.fill();
+            context.restore();
+        }
+
+        // context.translate(100, 400);
+        // context.beginPath();
+        // context.arc(0, 0, 50, 0, Math.PI * 2);
+        // context.fill();
     };
 };
 
