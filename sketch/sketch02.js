e6aac51f8c2f646c573ab3b6b3b017e2da6f190c
Nitish Kumar <krnitish@live.in>
2023-01-13 23:04:31 +0530

    Creative Coding: Arch

--- sketch/sketch02.js
+++ sketch/sketch02.js
@@ -32,11 +32,28 @@ const sketch = () => {
             context.save();
             context.translate(x, y);
             context.rotate(-angle);
-            context.scale(random.range(1, 3), 1);
+            context.scale(random.range(1, 3), random.range(0.2, 0.5));
 
             context.beginPath();
-            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
             context.fill();
+            context.restore();
+
+            context.save();
+            context.translate(cx, cy);
+            context.rotate(-angle);
+
+            context.beginPath();
+            context.lineWidth = random.range(5, 20);
+            context.arc(
+                0,
+                0,
+                radius * random.range(0.7, 1.3),
+                slice * random.range(0, -8),
+                slice * random.range(1, 5)
+            );
+            context.stroke();
+
             context.restore();
         }
 
